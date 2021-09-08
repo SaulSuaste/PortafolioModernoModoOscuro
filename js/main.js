37340e2b7cbf5a3f998ee3c17d770adae916d6bd
Saul Suaste <sumsaul94@hotmail.com>
2021-09-08 15:16:16 -0500

    Escolaridad y Trabajos, html,css,js

--- js/main.js
+++ js/main.js
@@ -47,3 +47,34 @@ function toggleSkills(){
 skillsHeader.forEach(element=>{
     element.addEventListener("click", toggleSkills)
 });
+
+// ============ Pestañas de aptitudes ==========================
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click", ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabCon =>{
+            tabCon.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+
+        tabs.forEach(tab =>{
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+
+    });
+    
+});
+
+
+
+
+
+
+
+
+
