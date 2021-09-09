0f44dca28e35d969854107b68f8544e586266160
Saul Suaste <sumsaul94@hotmail.com>
2021-09-08 21:59:38 -0500

    Services (html,css,js)

--- js/main.js
+++ js/main.js
@@ -71,6 +71,32 @@ tabs.forEach(tab =>{
 });
 
 
+// ============ Services modal ==========================
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add("active-modal");
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener("click", ()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener("click", ()=>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove("active-modal")
+        })
+    })
+})
+
+
+
+
 
 
 
